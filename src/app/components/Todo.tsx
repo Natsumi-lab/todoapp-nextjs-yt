371267b0c1574d0b542275b13f01b70c5558039d
Natsumi-lab <imustan81@gmail.com>
2025-12-29 16:40:21 +0900

    initial commit

---
+++ src/app/components/Todo.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { editTodo, deleteTodo } from "@/api";
+import { Task } from "@/types";
+import React, { useState, useEffect, useRef } from 'react'; 
+import { useRouter } from "next/navigation";
+
+interface TodoProps {
+    todo: Task;
+}
+
+const Todo = ({todo}: TodoProps) => {
+    const router = useRouter();
+    const ref = useRef<HTMLInputElement>(null);
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTaskTitle, setEditedTaskTitle] = useState(todo.text);
+
+useEffect(() => {
+    if(isEditing){
+        ref.current?.focus();
+    }
+},[isEditing]);
+
+const handleEdit = async () => {
+    setIsEditing(true);
+};
+
+const handleSave = async () => {
+    await editTodo(todo.id, editedTaskTitle);
+    setIsEditing(false);
+    router.refresh();
+};
+
+const handleDelete = async () => {
+    await deleteTodo(todo.id);
+    router.refresh();
+};
+
+return (
+        <li 
+            key={todo.id} 
+            className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded-md shadow-md"
+        >
+            {isEditing ? (
+              <input 
+                ref={ref}
+                type="text" 
+                className="mr-2 py-1 px-2 rounded border-gray-400 border" defaultValue={todo.text} 
+                value={editedTaskTitle}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditedTaskTitle(e.target.value)
+                }
+              /> 
+            ): (
+              <span>{todo.text}</span> 
+            )}            
+            <div>
+                {isEditing ? (
+                    <button className="text-blue-500 mr-3" onClick={ handleSave }>
+                        save
+                    </button>
+                    ) : (
+                    <button className="text-green-500 mr-3" onClick={ handleEdit }>
+                        edit
+                    </button>
+                )}  
+                <button className="text-red-500 mr-3" onClick={ handleDelete }>Delete</button>
+            </div>
+        </li>
+);
+};
+
+export default Todo;
